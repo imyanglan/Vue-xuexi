afef66405ef3caa1f909a7ab2696214b5dcafda0
imyarnlan <2530381004@qq.com>
2020-01-07 16:57:23 +0800

    vue.js更新36

---
+++ LearnVuejs/Day5-1（webpack的使用）/04-webpack配置Vue/src(源码-开发的东西都放这里)/main.js
@@ -0,0 +1,41 @@
+//在这里导入
+// 1.使用commonJS的模块化规范
+const {add,mul}=require('./js/mathUtils');
+console.log(add(20, 30));
+console.log(mul(20, 30));
+// 现在就是以模块化的想法用我们的代码
+
+
+// 2.使用ES6的模块化规范
+// 我们来看看这样调用，通过webpack能不能使用
+import {name,age,height} from "./js/info";
+// 它会自动跳出后面的代码 而且在这里面我们不用写.js webpack它会自动帮我们找到这个文件，
+// 在前面前端模块化调用的时候我们需要加上.js浏览器才会解析
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+// 这里我们写完之后还需要重新再打包一次
+
+// 3.依赖css文件      把它也当成一个模块 通过require去加载这个模块
+require('./css/normal.css')
+// 这里我们也不需要写变量，我们在上面写变量的原因是之后要用到，而样式我们只需要依赖就行了
+// 如果我们直接打包的话，会报错，因为webpack不能直接对css文件进行解析，我们需要通过合适的loader
+// 对该文件进行处理  我们去到官网里可以直接看如何使用合适的loader
+
+// 4.依赖less文件
+require('./css/special.less')
+document.writeln('<h2>你好啊，less</h2>')
+// 动态写入html
+
+
+// 5.使用Vue进行开发
+import Vue from 'vue'
+// 使用之前我们得先依赖
+new Vue({
+  el:'#app',
+  data:{
+    message:'Hello World'
+  }
+})
---
+++ LearnVuejs/Day5-1（webpack的使用）/04-webpack配置Vue/webpack.config.js
@@ -0,0 +1,97 @@
+const path=require('path')
+
+module.exports={
+  entry:'./src(源码-开发的东西都放这里)/main.js',
+  output:{
+    path:path.resolve(__dirname,'dist（distribution-发布-打包的东西都放这里）'),
+    filename:'bundle.js',
+    publicPath:'./dist（distribution-发布-打包的东西都放这里）/' //只要加上这个，
+    // 以后任何涉及到url的东西它会自动在前面加上dist/这个地址
+  },
+  // 按照webpack中文文档中css-loader安装好之后我们要在这里进行配置
+  module: {
+    rules: [
+      {
+        test: /\.css$/,  //匹配所有的css文件
+        use: [ 'style-loader', 'css-loader' ]
+        // css-loader只负责将css文件进行加载  它是不负责帮你解析的 也不负责帮你把在css里
+        // 写的东西放在HTML里让它生效  如果想让它生效我们得再安装一个loader
+        // 就是style-loader
+        // style-loader负责将样式添加到DOM中生效
+        // 在webpack中它在读取多个loader的时候是从右到左读的，所以顺序一定不能放错
+      },
+      {//从官网复制过来
+        test: /\.less$/,
+        // 这下面也可以按照数组的方式写  如上
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS  顺序也很重要不要弄错
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 当加载的图片，小于limit时，会将图片编译成base64字符串形式。
+              //当webpack在帮助我们加载图片的时候它会在这里看一下我们的图到底是大于limit
+              // 还是小于limit 如果是小于limit它会直接用url对它进行加载 并且将它编译成
+              // base64的形式  而如果你是大于limit的话 它会使用另外一个的东西
+              // 就是file-loader来对我们的图片进行加载
+              //而base64不需要单独的文件来存储 因为它是base64的字符串所以不需要单独的文件来处理
+              // 但是如果你要将图片作为图片来加载的话 这个图片就要进行打包 那这张图片就要打包到
+              // 我们dist文件夹里 到时候发布的时候这些图片连同里面的bundle.js一起进行发布
+              limit: 14000,
+              name:'img/[name].[hash:8].[ext]'
+            }
+            // 这个name就是用于我们的图片进行命名的
+            // 如果我们要给它原来的名字的话我们要加中括号[]里面再写个name 这是它固定的语法
+            // 这样才会取那个图片原来的名字，如果不加括号只写个name它就是个固定值
+            // 出来的图片就是name.jpg  就是个写死的名字 变量要使用中括号
+            // 这里的.就是将这些东西连接起来的
+            // hash:8 是为了防止图片同名 ext就是扩展名
+
+          }
+        ]},
+
+      {
+        test: /\.js$/,
+        //exclude:排除
+        //include:包含
+        exclude: /(node_modules|bower_components)/,
+        // 这里就是表示node_modules这个文件夹里的东西
+        // 和bower_components这个文件夹里的东西不用转化
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 这是配置 就我们刚刚安装的时候最后那一行的配置
+            // presets: ['@babel/preset-env']
+            // 这里的配置我们不用官网里的默认配置而是我们刚刚装的那个
+            presets: ['es2015']
+
+          }
+        }
+      }
+
+    ]
+  },
+  // 将这几行代码从文档中复制过来就行了
+  resolve:{
+    // alias:别名
+    alias:{
+      'vue$':'vue/dist/vue.esm.js'
+      // 它是这样指定的  这样指定是什么意思
+      // 当我们在代码中做了这个操作的时候 import Vue from 'vue'
+      // 它就会先来看一下这个vue有没有先指向一个具体的文件夹
+      // 而我们在这里给它指向了一个具体的文件夹
+      // 这个时候它就不会按照默认的方式去找我们的vue文件了
+      // 然后我们指定的这个文件中就包含我们的runtime-compiler
+      // 这样就选择使用了第二种方式：runtime-compiler
+    }
+ }
+
+}
