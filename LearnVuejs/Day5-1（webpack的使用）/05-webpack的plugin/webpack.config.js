539ff99069d28bbd631044a662a8b43d43f18717
imyanglan <2530381004@qq.com>
2020-01-13 16:43:55 +0800

    vue.js更新40

--- LearnVuejs/Day5-1（webpack的使用）/05-webpack的plugin/webpack.config.js
+++ LearnVuejs/Day5-1（webpack的使用）/05-webpack的plugin/webpack.config.js
@@ -115,11 +115,13 @@ module.exports={
   plugins:[
     new webpack.BannerPlugin('最终版权归imyanglan所有'),
     // 来到这个位置引用上面导入进来的插件就行了
-    // 如何使打包的html文件使用模板呢？
-    new HtmlWebpackPlugin(
-        // 在这里面传入一个参数
 
-    )
+    // 如何使打包的html文件使用模板呢？
+    new HtmlWebpackPlugin({
+        // 在这里面传入一个参数
+        template:'index.html'
+      // 这个就是我们要打包的那个html文件  用它作为模板
+    })
   ]
 
 
