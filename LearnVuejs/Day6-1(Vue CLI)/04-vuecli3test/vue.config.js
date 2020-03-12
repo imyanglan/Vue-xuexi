e1761e6aaa117f56e2d506cd9a75e242f617e658
imyanglan <2530381004@qq.com>
2020-03-12 13:43:42 +0800

    更新49

--- LearnVuejs/Day6-1(Vue CLI)/04-vuecli3test/vue.config.js
+++ LearnVuejs/Day6-1(Vue CLI)/04-vuecli3test/vue.config.js
@@ -1,4 +1,16 @@
-module.exports={
-
-
-}
+// const path = require('path')
+// function resolve (dir){
+//   return path.join(__dirname,dir)
+// }
+//
+// module.exports={
+//   // 1.基础的配置方式
+//   configureWebpack:{
+//     resolve:{
+//       alias:{
+//         'components':'@/components',
+//         'pages':'@/pages'
+//       }
+//     }
+//   }
+// }
