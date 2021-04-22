befac9aaf1ae7ecbb6cb7882ff27bcc3986ac8cc
soda <sodayarn@163.com>
2021-04-22 18:56:59 +0800

    53[路由传递参数]

--- LearnVuejs/Day7-1（Vue-Router）/vuerouter/src/App.vue
+++ LearnVuejs/Day7-1（Vue-Router）/vuerouter/src/App.vue
@@ -6,11 +6,15 @@
     <router-link to="/about">关于</router-link>
     <!-- 这里我们还需要一个组件用来渲染我们要展示的组件是放在什么位置的 相当于占位
      当我们选择什么路径时更具路径的改变，它会将组件的内容替换到router-view的位置上-->
-    <router-link :to="'/user/'+userId">用户</router-link>
+    <!-- <router-link :to="'/user/'+userId">用户</router-link> -->
     <!-- <router-link to="/profile">档案</router-link>  如果想额外的传递一些
     东西的话 不能在这里传递字符串而必须是一个对象 传递对象时必须得加绑定:不然它不知道这个要作为对象去读取
     传递更多数据就是在query里传-->
-    <router-link :to="{path:'/profile',query:{name:'imyanglan',age:'18',}}">档案</router-link>
+    <!-- <router-link :to="{path:'/profile',query:{name:'imyanglan',age:'18',}}">档案</router-link> -->
+  
+    <button @click="userClick()">用户</button>
+    <button @click="profileClick()">档案</button>
+    <!-- 监听点击 -->
     <router-view></router-view>
   </div>
 </template>
@@ -24,6 +28,16 @@ export default {
     return{
       userId:'imyanglan',
     }
+  },
+  methods:{
+    userClick(){
+      // 用代码进行页面跳转
+      this.$router.push('/user/'+this.userId)
+    },
+    profileClick(){
+      // 用代码进行页面跳转传参
+      this.$router.push({path:'/profile',query:{name:'imyanglan',age:'18',}})
+    },
   }
 }
 </script>
--- LearnVuejs/Day7-1（Vue-Router）/vuerouter/src/components/Profile.vue
+++ LearnVuejs/Day7-1（Vue-Router）/vuerouter/src/components/Profile.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
         <h2>我是Profile组件</h2>
-        <h2>{{$router.query.name}}</h2>
-        <h2>{{$router.query.age}}</h2>
+        <h2>{{$route.query.name}}</h2>
+        <h2>{{$route.query.age}}</h2>
         <!-- 这里之所以是.query是因为url协议就是这样命名的是为了规范 所以之前在user里用params也是 -->
     </div>
 </template>
 
 <script>
 export default {
-
+    name:'Profile'
 }
 </script>
 <style>
